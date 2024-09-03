227cbf072d93e61b551a5de8e49c5403d64c9e8d
agdc101 <agdc101@gmail.com>
2024-09-03 21:37:01 +0100

    live api route added for subdomain

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
-    uri: 'http://161.35.163.106/api', //change for production
+    uri: 'https://admin.thecompanyoflandscapers.co.uk/api', //change for production
 });
 
 // Create a middleware link to add the Authorization header
